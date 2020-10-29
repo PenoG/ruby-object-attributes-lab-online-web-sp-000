caf7510203d7e85bd3eab4e419f17fcca8e12e92
Mason Phillips <mjphillips2000@gmail.com>
2020-10-29 06:51:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,19 +1,19 @@
 class Dog
   
-  def name=(dog_name)
-    @this_dogs_name = dog_name
-  end
-  
   def name
-    @this_dogs_name
+    @name
   end
-  
-  def initialize(breed)
-    @breed = breed
+
+  def name=(name)
+    @name = name
   end
-  
+
   def breed
     @breed
+  end
+
+  def breed=(breed)
+    @breed = breed
   end
   
 end
